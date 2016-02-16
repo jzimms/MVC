af66ddf5d99e57dac69bb6954913db58e35e20ca
jzimms <jzimms@comcast.net>
2016-02-15 23:53:35 -0500

    Apress
    Stuff that I have done with Apress

---
+++ LanguageFeatures/LanguageFeatures/Models/MyAsynchMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsynchMethods
+    {
+        //public static Task<long?> GetPageLength()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    var httpTask = client.GetAsync("http://apress.com");
+        //    return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => { return antecedent.Result.Content.Headers.ContentLength; });
+        //}
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://www.google.com");
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private string name;
+
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get { return ProductID + name; } set { name = value; } }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
